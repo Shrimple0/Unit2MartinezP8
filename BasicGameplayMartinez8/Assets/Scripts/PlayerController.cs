b791f2b1fbe1174be7bf03afae68b05251e75a7e
Shrimple0 <756116@student.dusd.net>
2025-03-24 15:27:59 -0700

    programmed projectile

--- BasicGameplayMartinez8/Assets/Scripts/PlayerController.cs
+++ BasicGameplayMartinez8/Assets/Scripts/PlayerController.cs
@@ -12,8 +12,26 @@ public class PlayerController : MonoBehaviour
 
     // Update is called once per frame
     public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
     void Update()
     {
-        horizontalInput = Input.GetAxis(“Horizontal”), 
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        // Keep the player in bounds
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right *  horizontalInput * Time.deltaTime * speed);
     }
 }
